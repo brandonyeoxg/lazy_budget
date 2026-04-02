d4533255644bf995a63d0a26a1b8280d47b83ef7
brandonyeoxg <brandonyeoxg@gmail.com>
2026-04-03 00:38:00 +0800

    feat(args): add args parsing

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--file", help="bank statement in pdf")
+parser.add_argument("--folder", help="folder to process the entire statements")
+
+
 def main():
-    print("Hello from lazy-budget!")
+    args = parser.parse_args()
+    print(f"got file: {args.file}")
 
 
 if __name__ == "__main__":
